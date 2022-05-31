1d6967e28456a84eb5a24b4df3eafddf6c46bbbb
Nazar Garipov <nazar.garipov475@gmail.com>
2022-05-31 11:44:57 +0500

    Refactor

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import json
 import random
 import socket
 import threading
-from trace import Tracer
 
-from flask import Flask, Response, render_template, request, url_for
-from markupsafe import escape
+from flask import Flask, render_template, request
+
+from trace import Tracer
 
 app = Flask(__name__)
 
@@ -15,14 +15,16 @@ def index():
     return render_template('index.html')
 
 
+is_finished = {}
 trace_result = {}
 
 
 def start_tracing(tracer: Tracer, id: int):
     trace_result[id] = []
+    is_finished[id] = False
     for i in tracer.start():
         trace_result[id].append(i)
-        print(len(trace_result[id]))
+    is_finished[id] = True
 
 
 @app.route('/trace', methods=['GET'])
@@ -38,8 +40,16 @@ def search():
     return str(id)
 
 
+class Result:
+    def __init__(self, data, is_finished):
+        self.data = data
+        self.is_finished = is_finished
+
+
 @app.route('/get-state/<int:id>')
 def get_state(id: int):
     while id not in trace_result or len(trace_result[id]) == 0:
         pass
-    return json.dumps(trace_result[id].pop(0), default=lambda x: x.__dict__)
+    result = Result(trace_result[id][:], is_finished[id])
+    trace_result[id] = []
+    return json.dumps(result, default=lambda x: x.__dict__)
--- trace.py
+++ trace.py
@@ -9,7 +9,6 @@ def get_info(ip) -> (str, str):
     try:
         info = json.loads(
             requests.get(f"https://ipinfo.io/{ip}/json").content)
-        # return f'{info.get("org", ""):<30}{info.get("country", ""):<5}'
         return info.get("org", ""), info.get("country", "")
     except Exception:
         return "*****"
@@ -27,14 +26,14 @@ class TraceResult:
 
 
 class Tracer:
-    def __init__(self, max_hops: int, destination_ip: str, port: int, timeout_in_ms: int):
+    def __init__(self, max_hops: int, destination_ip: str, port: int,
+                 timeout_in_ms: int):
         self._max_hops = max_hops
         self._destination = destination_ip
         self._port = port
         self._timeout = timeout_in_ms / 1000
 
     def start(self):
-        # log('tracing: {}'.format(self._destination))
         port = 33434
         try:
             with socket.socket(
@@ -62,29 +61,28 @@ class Tracer:
                     pass
                 if answer_from:
                     ip_info = get_info(answer_from[0])
-                    yield TraceResult(ttl, answer_from[0], ip_info[1], ip_info[0])
-                #  log('{:<3}{:<20}{:<25}'.format(ttl, answer_from[0], ip_info))
+                    yield TraceResult(ttl, answer_from[0], ip_info[1],
+                                      ip_info[0])
                 else:
                     yield TraceResult(ttl)
-                    #       log('{}\t *****'.format(ttl))
                     continue
                 if answer_from[0] == self._destination:
                     break
 
 
-
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser("Tracer with detecting country, autonomous system and provider")
-    parser.add_argument('-t', '--timeout', help='Timeout for ping (ms)', default=1000, type=int)
-    parser.add_argument('-m', '--max-hops', help='Max count of hops', default=30, type=int)
+    parser = argparse.ArgumentParser(
+        "Tracer with detecting country, autonomous system and provider")
+    parser.add_argument('-t', '--timeout', help='Timeout for ping (ms)',
+                        default=1000, type=int)
+    parser.add_argument('-m', '--max-hops', help='Max count of hops',
+                        default=30, type=int)
     args = parser.parse_args()
     ip_or_domain = input('ip or domain: ')
     try:
         dest = socket.gethostbyname(ip_or_domain)
     except socket.error:
-        #    log(f"Can't resolve ip address for {ip_or_domain}. Check internet connection")
         exit()
     tracer = Tracer(args.max_hops, dest, 33343, args.timeout)
-    # trace(ip_or_domain, args.max_hops, args.timeout / 1000)
     for i in tracer.start():
         print(i)
